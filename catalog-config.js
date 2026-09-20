11167919e7aa9fa1ff52d998f4306009679d1fa7
mrchhangmail <mrchhan@gmail.com>
2026-09-21 02:34:18 +0500

    Update catalog-config.js

--- catalog-config.js
+++ catalog-config.js
@@ -11,12 +11,12 @@ window.CATALOG_CONFIG = {
   logo: 'icon-192.png',
 
   // WhatsApp number for orders: country code + number, digits only. Example: '923001234567'
-  whatsapp: '',
+  whatsapp: '923363330884',
 
   // Phone number shown to customers (tap to call). Example: '0300 1234567'. Leave '' to hide.
-  phone: '',
+  phone: '923363330884',
 
   address: 'F-319 Satellite Town',    // add your city here too
-  hours: '',                          // example: 'Mon to Sat, 9am to 8pm'
-  mapsUrl: ''                         // optional: paste your Google Maps share link
+  hours: 'Sat to Thur, 9am to 9pm',                          // example: 'Mon to Sat, 9am to 8pm'
+  mapsUrl: 'https://www.google.com/maps/place/Malik+Autoz/@33.6455277,73.0241933,13z/data=!4m10!1m2!2m1!1smalik+autoz!3m6!1s0x38df950018eac7b3:0x2a2f92bb3d7ec5fd!8m2!3d33.6455277!4d73.0592122!15sCgttYWxpayBhdXRvepIBEGF1dG9fcmVwYWlyX3Nob3DgAQA!16s%2Fg%2F11xzj_bx19?entry=ttu&g_ep=EgoyMDI2MDkxNi4wIKXMDSoASAFQAw%3D%3D'                         // optional: paste your Google Maps share link
 };
